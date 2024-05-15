3ab157d5783cbc000253ebf4f5c444fc5c8da2e3
Hafsa Sajid <139751784+its-hafsa04@users.noreply.github.com>
2024-05-15 20:19:42 +0500

    Time: 5 ms (28.68%), Space: 8 MB (52.54%) - LeetHub

---
+++ 1666-make-the-string-great/1666-make-the-string-great.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> st;
+
+        for(char c: s){
+            if(!st.empty() && (st.top() == c+ 32 || st.top() ==c - 32)){
+                st.pop();
+            }else{
+                st.push(c);
+            }
+        }
+        string result;
+        while(!st.empty()){
+            result +=st.top();
+            st.pop();
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
