0857bc58f8884dfe67ee97c149d1670cd98362b5
Hafsa Sajid <139751784+its-hafsa04@users.noreply.github.com>
2024-05-28 12:26:58 +0500

    Time: 168 ms (11.17%), Space: 22.3 MB (95.8%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        for (int i = 0; i < heights.size(); i++) {
+
+            for (int j = 0; j < heights.size() - i - 1; j++) {
+                
+                if (heights[j] < heights[j + 1]) {
+                    swap(heights[j], heights[j + 1]);
+                    swap(names[j], names[j + 1]);
+                }
+            }
+        }
+        return names;
+    }
+};
