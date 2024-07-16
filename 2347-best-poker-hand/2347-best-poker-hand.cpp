e6488c5ac054be9d015f58b0993bcbf72e3e7def
Hafsa Sajid <139751784+its-hafsa04@users.noreply.github.com>
2024-07-16 20:35:14 +0500

    Time: 0 ms (100%), Space: 12.2 MB (5.54%) - LeetHub

---
+++ 2347-best-poker-hand/2347-best-poker-hand.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string bestHand(vector<int>& ranks, vector<char>& suits) {
+        unordered_set<int> s(suits.begin() , suits.end());
+        if(s.size() == 1)   return "Flush";
+        sort(ranks.begin() , ranks.end());
+        int mx = 0, count = 1;
+        for(int i = 1; i < 5; i++)
+        {
+            if(ranks[i] == ranks[i - 1])
+            {
+                count++;
+                mx = max(count , mx);
+            }
+            else
+                count = 1;
+        }
+        if(mx >= 3)  return "Three of a Kind";
+        if(mx == 2)  return "Pair";
+        return "High Card";
+    }
+};
