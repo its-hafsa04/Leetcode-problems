ad186d2a81ab1a997edaf83e1418b6d90efa8aa5
Hafsa Sajid <139751784+its-hafsa04@users.noreply.github.com>
2025-06-25 20:02:38 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -7,11 +7,11 @@ public:
         vector<int> ans(n);
         // prefix
         for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] * nums[i - 1];
+            prefix[i] = prefix[i - 1] * nums[i - 1]; // 1,1,2,6
         }
         // suffix
         for (int i = n - 2; i >= 0; --i) {
-            suffix[i] = suffix[i + 1] * nums[i + 1];
+            suffix[i] = suffix[i + 1] * nums[i + 1]; //1,4,12,24
         }
         // ans
         for (int i = 0; i < n; i++) {
