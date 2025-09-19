8a24ca354c7e393d23b6eaa15f6ff995a917a6ac
Hafsa Sajid <139751784+its-hafsa04@users.noreply.github.com>
2025-09-19 19:21:26 +0500

    Time: 11 ms (51.3%), Space: 11.4 MB (83.57%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-       if (s.empty() || t.empty()) {
-        return ""
-    }
-    unordered_map<char, int> t_freq;
-    for (char c : t) {
-        t_freq[c]++;
-    }
-    int left = 0, right = 0;
-    int min_len = INT_MAX;
-    int start = 0;
-    int count = 0;
-    while (right < s.size()) {
-        char c = s[right];
-        if (t_freq.count(c) > 0) {
-            t_freq[c]--;
-            if (t_freq[c] >= 0) {
-                count++;
-            } 
+        if (s.empty() || t.empty()) {
+            return "";
         }
-        right++;
-        while (count == t.size()) {
-            if (right - left < min_len) {
-                min_len = right - left;
-                start = left;
-            }
-            char c = s[left];
-            if (t_freq.count(c) > 0) {
-                t_freq[c]++;
-                if (t_freq[c] > 0) {
-                    count--;
+        unordered_map<char, int> freq;
+        for (char ch : t) {
+            freq[ch]++;
+        }
+        int left = 0, right = 0;
+        int min_len = INT_MAX;
+        int start = 0;
+        int count = 0;
+        while (right < s.size()) {
+            char ch = s[right];
+            if (freq.count(ch) > 0) {
+                freq[ch]--;
+                if (freq[ch] >= 0) {
+                    count++;
                 }
             }
-            left++;
+            right++;
+            while (count == t.size()) {
+                if (right - left < min_len) {
+                    min_len = right - left;
+                    start = left;
+                }
+                char ch = s[left];
+                if (freq.count(ch) > 0) {
+                    freq[ch]++;
+                    if (freq[ch] > 0) {
+                        count--;
+                    }
+                }
+                left++;
+            }
         }
-    }
-    return min_len == INT_MAX ? "" : s.substr(start, min_len);
+        return min_len == INT_MAX ? "" : s.substr(start, min_len);
     }
 };
