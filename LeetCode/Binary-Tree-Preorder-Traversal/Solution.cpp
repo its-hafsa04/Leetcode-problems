b2930e55c3181f11c383a06188e99469c398eaab
Hafsa Sajid <139751784+its-hafsa04@users.noreply.github.com>
2025-10-23 21:21:59 +0500

    Add solution for Binary Tree Preorder Traversal

--- LeetCode/Binary-Tree-Preorder-Traversal/Solution.cpp
+++ LeetCode/Binary-Tree-Preorder-Traversal/Solution.cpp
@@ -6,25 +6,24 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         if (root == NULL)
-            return {};  // Empty tree → return empty list
-
+            return {};
         vector<int> ans;
         preorder(root, ans);
         return ans;
     }
-
     void preorder(TreeNode* root, vector<int>& ans) {
-        if (root == NULL) return;
-
-        ans.push_back(root->val);     // Visit root
-        preorder(root->left, ans);    // Recurse left
-        preorder(root->right, ans);   // Recurse right
+        if (root == NULL)
+            return;
+        ans.push_back(root->val);
+        preorder(root->left, ans);
+        preorder(root->right, ans);
     }
 };
