b3f0acf1c25f3de474a83f4672dfe1cc0cfc2086
Hafsa Sajid <139751784+its-hafsa04@users.noreply.github.com>
2025-11-04 19:15:21 +0500

    Add solution for Sqrt(x)

--- LeetCode/Sqrt(x)/Solution.cpp
+++ LeetCode/Sqrt(x)/Solution.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0)
-            return x;
-        int first = 1, last = x;
-        while (first <= last) {
-            int mid = first + (last - first) / 2;
-            // mid * mid == x gives runtime error
-            if (mid  == x / mid)
-                return mid;
-            else if (mid > x / mid) {
-                last = mid - 1;
-            }
-            else {
-                first = mid + 1;
-            }
+        if(x==0) return x;
+        int first=1, last=x;
+        while(first<= last){
+            int mid = first+(last-first)/2;
+            if(mid==x/mid) return mid;
+            else if(mid>x/mid) last =mid-1;
+            else first =mid+1;
         }
         return last;
     }
