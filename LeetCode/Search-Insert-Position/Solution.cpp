7b45f0729b966aed2c72bfc6b61eb6d217df6a47
Hafsa Sajid <139751784+its-hafsa04@users.noreply.github.com>
2025-11-04 18:59:24 +0500

    Add solution for Search Insert Position

--- LeetCode/Search-Insert-Position/Solution.cpp
+++ LeetCode/Search-Insert-Position/Solution.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        
         int size = nums.size();
-        
         int ans;
-        int left = 0; int right = size-1;
-        while(left <= right){
-            
-            int mid = left + (right - left)/2;
-            if(nums[mid] >= target){
-                right = mid-1;
+        int left = 0, right = size - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
                 ans = mid;
-            } 
-            else left = mid+1;
+            } else
+                left = mid + 1;
         }
         return ans;
     }
