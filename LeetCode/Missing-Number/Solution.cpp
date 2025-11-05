8c28cef73c0bc270741aba49252e2ac540614a1b
Hafsa Sajid <139751784+its-hafsa04@users.noreply.github.com>
2025-11-05 19:34:54 +0500

    Add solution for Missing Number

--- LeetCode/Missing-Number/Solution.cpp
+++ LeetCode/Missing-Number/Solution.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) { // find the missing number in a array
+    int missingNumber(vector<int>& nums) {
         int sum = 0; // sum of all numbers initially 0
         int total = nums.size()*(nums.size() + 1)/2; // total is the sum of all numbers in the array
         for (auto number : nums) { // iterate through the array
@@ -8,4 +8,5 @@ public:
         }
         return total - sum; // return the difference between the sum and the total that is the missing number
     }
+    
 };
