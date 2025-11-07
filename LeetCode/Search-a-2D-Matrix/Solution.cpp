1f42931781342b8a53a675c0604327fa7b3d3247
Hafsa Sajid <139751784+its-hafsa04@users.noreply.github.com>
2025-11-07 18:38:49 +0500

    Add solution for Search a 2D Matrix

--- LeetCode/Search-a-2D-Matrix/Solution.cpp
+++ LeetCode/Search-a-2D-Matrix/Solution.cpp
@@ -1,1 +1,20 @@
-    O(log(n*m)) ≈ O(log n + log m)
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int st = 0 , end = n * m - 1;
+        
+        // Treat the matrix like a 1D array
+        while(st <= end){
+            int mid = (end - st) / 2 + st;
+            // Map mid to 2D index: row = mid/m, col = mid%m
+            int search = matrix[mid/m][mid%m];
+            if(search == target) return true;
+            if(search > target){
+                end = mid-1;
+            }else st = mid+1;
+        }
+        return false;
+    }
+};
