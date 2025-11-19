94afdbb2dc431a5e1b77fe345d8963cb961b3214
Hafsa Sajid <139751784+its-hafsa04@users.noreply.github.com>
2025-11-19 21:56:58 +0500

    Add solution for Search in Rotated Sorted Array

--- LeetCode/Search-in-Rotated-Sorted-Array/Solution.cpp
+++ LeetCode/Search-in-Rotated-Sorted-Array/Solution.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int size = nums.size();
-        int left = 0;
-        int right = size-1;
+    int findPivot(vector<int>& v){
+        int s=0,e=v.size()-1;
+        while(s<e){
+            int mid = s+(e-s)/2;
+            if(mid+1 < v.size() && v[mid+1]<v[mid]) return mid;
+            if(mid-1 >= 0 && v[mid-1] > v[mid]) return mid-1;
+            if(v[s] >= v[mid]) e=mid-1;
+            else s=mid+1;
+        }
+        return s;
+    }
 
-        while(left <= right){
-            
-            int mid = left + (right-left)/2;
-            if(nums[mid]==target) return mid;
-
-            if(nums[mid] >= nums[left]){
-                if(nums[left] <= target && target <= nums[mid]){
-                    right = mid-1;
-                }
-                else{
-                    left = mid+1;
-                }
-            }
-            else{
-                if(nums[mid] <= target && target <= nums[right]){
-                    left = mid+1;
-                }
-                else{
-                    right = mid-1;
-                }
-            }
+    int binary(vector<int>& v,int s,int e,int t){
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(v[mid] == t) return mid;
+            else if(v[mid] > t) e=mid-1;
+            else s=mid+1;
+        }
+        return -1;
+    }
+    int search(vector<int>& nums, int t) {
+        int pivotIdx = findPivot(nums);
+        cout << pivotIdx << "  " << endl;
+        if(pivotIdx==-1) return -1;
+        //cout << pivotIdx << "  " << nums[pivotIdx] << endl;
+        // return -1;
+        if(nums[0]<=t && nums[pivotIdx]>=t){
+            return binary(nums,0,pivotIdx,t);
+        }else{
+            return binary(nums,pivotIdx+1,nums.size()-1,t);
         }
         return -1;
     }
