4c9994610014820d6a984514f886016291bcb311
Hafsa Sajid <139751784+its-hafsa04@users.noreply.github.com>
2026-05-25 14:19:34 +0500

    Add solution for Palindrome Number

--- LeetCode/Palindrome-Number/Solution.cpp
+++ LeetCode/Palindrome-Number/Solution.cpp
@@ -1,20 +1,13 @@
-1class Solution {
-2public:
-3    bool isPalindrome(int x) {
-4        if (x < 0) {
-5            return false;
-6        }
-7        if (x != 0 && x % 10 == 0) {
-8            return false;
-9        }
-10        int reverse = 0;
-11        int temp = x;
-12        while (temp > reverse) {
-13            int pop = temp % 10;
-14            reverse = reverse * 10 + pop;
-15            temp /= 10;
-16        }
-17        return (temp == reverse) || (temp == reverse / 10);
-18    }
-19};
-20
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;  // negatives are not palindrome
+
+        long long rev = 0, num = x;
+        while(num) {
+            rev = rev * 10 + num % 10;  // build reversed number
+            num /= 10;
+        }
+        return rev == x;  // check if equal
+    }
+};
