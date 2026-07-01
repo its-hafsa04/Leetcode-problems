688015de444e5afaf619a2b72a18c4261cab60d5
Hafsa Sajid <139751784+its-hafsa04@users.noreply.github.com>
2026-07-01 22:42:54 +0500

    Add solution for Find the Index of the First Occurrence in a String

--- LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.cpp
+++ LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.cpp
@@ -1,14 +1,1 @@
-1class Solution {
-2public:
-3    int strStr(string haystack, string needle) {
-4        if(haystack.size()<needle.size()){
-5            return -1;
-6        }
-7        for(int i=0; i<= haystack.size() - needle.size(); i++){
-8            if(haystack.substr(i, needle.size()) == needle){
-9                return i;
-10            }
-11        }
-12        return -1;
-13    }
-14};
+haystack = "abcxyz", needle = "xyz"
